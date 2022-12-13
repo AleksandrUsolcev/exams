ab41fa55d39bba63d1c08c682d51347281ba1859
Aleksandr Usolcev <alexusolev@gmail.com>
2022-12-13 17:10:13 +0300

    admin panel update

--- quizzes/questions/admin.py
+++ quizzes/questions/admin.py
@@ -9,19 +9,36 @@ class QuizThemeAdmin(admin.ModelAdmin):
     list_editable = ('priority',)
 
 
+class QuestionInline(admin.StackedInline):
+    model = Question
+    show_change_link = True
+    extra = 0
+
+
+class VariantInline(admin.TabularInline):
+    model = Variant
+    show_change_link = True
+    extra = 0
+
+
 class QuizAdmin(admin.ModelAdmin):
     list_display = ('title', 'theme', 'author', 'revision', 'created')
     readonly_fields = ('slug', 'revision')
+    raw_id_fields = ('author',)
+    inlines = (QuestionInline,)
 
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('text', 'quiz', 'priority')
     list_editable = ('priority',)
+    raw_id_fields = ('quiz',)
+    inlines = (VariantInline,)
 
 
 class VariantAdmin(admin.ModelAdmin):
     list_display = ('text', 'question', 'correct', 'priority')
     list_editable = ('correct', 'priority')
+    raw_id_fields = ('question',)
 
 
 class ProgressAdmin(admin.ModelAdmin):
