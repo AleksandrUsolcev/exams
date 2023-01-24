54d5c83fba22c613a75f883a9f963378ed15c8d3
Aleksandr Usolcev <alexusolev@gmail.com>
2023-01-24 09:02:37 +0300

    disable signals for loaddata

--- exams/exams/signals.py
+++ exams/exams/signals.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from django.db.models import Count, Q
 from django.db.models.signals import post_delete, post_save, pre_save
 from django.dispatch import receiver
@@ -6,8 +8,18 @@ from django.utils import timezone
 from .models import Exam, Question, Variant
 
 
+def disable_for_loaddata(signal_handler):
+    @wraps(signal_handler)
+    def wrapper(*args, **kwargs):
+        if kwargs.get('raw'):
+            return
+        signal_handler(*args, **kwargs)
+    return wrapper
+
+
 @receiver(post_save, sender=Question)
 @receiver(post_delete, sender=Question)
+@disable_for_loaddata
 def exam_update_revision(sender, instance, **kwargs):
     if (instance.exam.active and instance.exam.visibility):
         instance.exam.revision = timezone.now()
@@ -16,6 +28,7 @@ def exam_update_revision(sender, instance, **kwargs):
 
 @receiver(post_save, sender=Variant)
 @receiver(post_delete, sender=Variant)
+@disable_for_loaddata
 def question_active_change(sender, instance, **kwargs):
     empty_status = instance.exam.empty_answers
     active = False
@@ -35,6 +48,7 @@ def question_active_change(sender, instance, **kwargs):
 
 @receiver(post_save, sender=Question)
 @receiver(post_delete, sender=Question)
+@disable_for_loaddata
 def exam_active_change(sender, instance, **kwargs):
     empty_status = instance.exam.empty_answers
     active = False
@@ -61,6 +75,7 @@ def exam_active_change(sender, instance, **kwargs):
 
 
 @receiver(pre_save, sender=Exam)
+@disable_for_loaddata
 def exam_empty_answers_change(sender, instance, **kwargs):
     if instance.id is None:
         return
@@ -89,6 +104,7 @@ def exam_empty_answers_change(sender, instance, **kwargs):
 
 
 @receiver(post_save, sender=Exam)
+@disable_for_loaddata
 def exam_active_self_change(sender, instance, **kwargs):
     active = False
     questions = Question.objects.filter(
