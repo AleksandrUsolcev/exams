064bcbbe93f7e816fc699483fef0d7d774a10d41
Aleksandr Usolcev <alexusolev@gmail.com>
2023-01-24 17:30:40 +0300

    exam details, signup form validation

--- exams/users/forms.py
+++ exams/users/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ValidationError
 
@@ -10,10 +12,13 @@ class SignupForm(UserCreationForm):
         fields = ('email', 'username')
 
     def clean(self):
-        email = self.cleaned_data.get('email').lower()
+        email = self.cleaned_data.get('email')
         username = self.cleaned_data.get('username')
-        if User.objects.filter(email=email):
+        if User.objects.filter(email__iexact=email):
             raise ValidationError('Указанный email уже занят')
+        if not re.match("^[A-Za-z0-9]*$", username):
+            raise ValidationError('Имя не соответствует требованиям '
+                                  '(только латиница и цифры)')
         if User.objects.filter(username__iexact=username):
             raise ValidationError('Пользователь с такими ником уже существует')
         return self.cleaned_data
